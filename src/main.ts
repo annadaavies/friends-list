b3e56856ace311bb6aebfff6d64712dc642eb17f
Horace Wong <horaceyin@hotmail.com>
2023-10-26 16:22:01 +0800

    change port back to 3000

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { friendModule } from './friend.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(friendModule);
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
 
